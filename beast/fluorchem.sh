2e527c17a2a904783a4b7eb10dce370edd539cd1
Connor McMillan <mibs510@gmail.com>
2019-01-15 22:20:27 -0800

    Added fluorchem.sh

--- beast/fluorchem.sh
+++ beast/fluorchem.sh
@@ -7,13 +7,13 @@ if [ $# -lt 1 ]; then
 	echo "${0} [target] [target] [target] [target] ..."
 	echo "Example: ${0} a b c d e f g h i j k l"
 	echo "Example: IMG=company_part#_version ${0} a b c d"
-	echo "IMG=biotechne_103-0039_v01 by default"
+	echo "IMG=fluorchem-mfg-master-2017-11-27 by default"
 	echo "IMG must be located in /home/partimag"
 	exit 1
 fi
 
 if [ "x${IMG}x" = "xx" ]; then
-	IMG=biotechne_103-0039_v01
+	IMG=fluorchem-mfg-master-2017-11-27
 fi
 
 if [ "$(mount | grep '/home/partimag')" == "" ]; then
@@ -36,7 +36,20 @@ echo "=================================================================="
 echo ""
 echo ""
 
-lsblk -o name,serial
+echo ""
+echo ""
+echo "=================================================================="
+echo " THE FOLLOWING DRIVES WILL BE IMAGED!!"
+echo "=================================================================="
+echo ""
+echo ""
+
+for i in "${@}"; do
+	lsblk -o name,serial | grep sd${i}
+
+done
+
+echo ""
 echo ""
 echo "Press Ctrl+C to exit"
 read -p "Press Enter to continue"
@@ -46,24 +59,18 @@ for i in "${@}"; do
 	echo ""
 	echo ""
 	echo "=================================================================================================================================="
-	echo "EXECUTING: sudo ocs-sr -batch -nogui -g auto -e1 auto -e2 -r -j2 -scr -p true restoredisk ${IMG} sd${i} | tee /tmp/sd${i}.txt"
+	echo "EXECUTING: pv {$IMG} > /dev/sd${i} && sudo xxhsum /dev/sd${i} &>> /tmp/xxhsum.log &"
 	echo "=================================================================================================================================="
 	echo ""
 	echo ""
-	sudo ocs-sr -batch -nogui -g auto -e1 auto -e2 -r -j2 -scr -p true restoredisk ${IMG} sd${i} | tee /tmp/sd${i}.txt
-done
-
-for i in "${@}"; do
-	echo "============================================"
-	echo "/dev/sd${i} completed: "
-	cat /tmp/sd${i}.txt | grep "Finished!"
-	echo "============================================"
+	sudo pv {$IMG} > /dev/sd${i} && sudo xxhsum /dev/sd${i} &>> /tmp/xxhsum.log &
 done
 
 echo ""
 echo ""
-echo "==========================================="
-echo " LOGS OF IMAGED DRIVES ARE LOCATED IN /tmp"
-echo "==========================================="
+echo "=================================================================="
+echo " ${IMG} has a sum of ade06eeaf7bcad46"
+echo " LOGS OF IMAGED DRIVES ARE LOCATED IN /tmp/xxhsum.log"
+echo "=================================================================="
 echo ""
 
