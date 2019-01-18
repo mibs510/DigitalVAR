5756df1ab01f310e4644e32ef6b0ede04d94165d
Connor McMillan <mibs510@gmail.com>
2019-01-18 11:37:52 -0800

    Fixes

--- server/ifupdownsucks.sh
+++ server/ifupdownsucks.sh
@@ -9,8 +9,8 @@ ETH1_IFACE="eth1"
 ETH1_MAC=$(cat /sys/class/net/$ETH1_IFACE/address)
 SERVER1_MAC="00:1e:67:cf:ee:8f"
 SERVER2_MAC="00:1e:67:e0:9d:6f"
-SERVER1_IP="192.168.168.1"
-SERVER2_IP="192.168.168.2"
+SERVER1_IP="192.168.1.241"
+SERVER2_IP="192.168.1.242"
 SERVERX_SUBNET="255.255.255.0"
 LOG_FILE=/var/log/ifupdownsucks.log
 
@@ -20,14 +20,14 @@ touch $LOG_FILE
 while true; do
 	ETH1_OPERSTATE=$(cat /sys/class/net/$ETH1_IFACE/operstate)
 	if [ "$ETH1_MAC" == "$SERVER1_MAC" ] && [ "$(pidof dhcpd)" != "" ]; then
-		echo "$(date +"%m/%d/%y@%r") -- This is server #1"
+		echo "$(date +"%m/%d/%y@%r") -- This is server #1" >> $LOG_FILE
 		if [ "$ETH1_OPERSTATE" == "up" ]; then
-			echo "$(date +"%m/%d/%y@%r") -- $ETH1_IFACE is up"
+			echo "$(date +"%m/%d/%y@%r") -- $ETH1_IFACE is up" >> $LOG_FILE
 			ETH1_IP=$(/sbin/ifconfig "$ETH1_IFACE" | grep 'inet ' | cut -d' ' -f10)
 			if [ "$ETH1_IP" == "" ]; then
 				echo "$(date +"%m/%d/%y@%r") -- $ETH1_IFACE doesn't have an IP address" >> $LOG_FILE
 			else
-				echo "$(date +"%m/%d/%y@%r") -- IP Address: $ETH1_IP"
+				echo "$(date +"%m/%d/%y@%r") -- IP Address: $ETH1_IP" >> $LOG_FILE
 			fi
 			if [ "$ETH1_IP" != "$SERVER1_IP" ]; then
 				echo "$(date +"%m/%d/%y@%r") -- Setting $ETH1_IFACE with an IP address of $SERVER1_IP" >> $LOG_FILE
@@ -40,12 +40,12 @@ while true; do
 	if [ "$ETH1_MAC" == "$SERVER2_MAC" ] && [ "$(pidof dhcpd)" != "" ]; then
 		echo "This is server #2"
 		if [ "$ETH1_OPERSTATE" == "up" ]; then
-			echo "$(date +"%m/%d/%y@%r") -- $ETH1_IFACE is up"
+			echo "$(date +"%m/%d/%y@%r") -- $ETH1_IFACE is up" >> $LOG_FILE
 			ETH1_IP=$(/sbin/ifconfig "$ETH1_IFACE" | grep 'inet ' | cut -d' ' -f10)
 			if [ "$ETH1_IP" == "" ]; then
 				echo "$(date +"%m/%d/%y@%r") -- $ETH1_IFACE doesn't have an IP address" >> $LOG_FILE
 			else
-				echo "$(date +"%m/%d/%y@%r") -- IP Address: $ETH1_IP"
+				echo "$(date +"%m/%d/%y@%r") -- IP Address: $ETH1_IP" >> $LOG_FILE
 			fi
 			if [ "$ETH1_IP" != "$SERVER2_IP" ]; then
 				echo "$(date +"%m/%d/%y@%r") -- Setting $ETH1_IFACE with an IP address of $SERVER2_IP" >> $LOG_FILE
